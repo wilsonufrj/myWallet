35160894ffda7a4dbc2b2de34b718c6a437df017
Wilson <waramos97@gmail.com>
2021-12-19 23:35:45 -0300

    Restruturacao do projeto

--- src/main/java/br/projeto/mywallet/Controller/SpendController.java
+++ src/main/java/br/projeto/mywallet/Controller/SpendController.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package br.projeto.mywallet.Controller;
 
 /**
  *
  * @author wilson
  */
-public class Wallet {
-    private Double allMoney;
-    
+public class SpendController {
+    //Recuperar todos os gastos
+    //Fazer um crud dos gastos
 }
--- src/main/java/br/projeto/mywallet/Model/Wallet.java
+++ src/main/java/br/projeto/mywallet/Model/Wallet.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Controller;
+package br.projeto.mywallet.Model;
 
 /**
  *
  * @author wilson
  */
-public class SpendController {
-    //Recuperar todos os gastos
+public class Wallet {
+    private Double allMoney;
+    
 }
